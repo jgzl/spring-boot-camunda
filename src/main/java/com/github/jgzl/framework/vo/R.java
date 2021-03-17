7b8859dca72dc74fff66e2c18e5bccd96144ab9d
lihaifeng <li7hai26@gmail.com>
2021-03-18 00:38:03 +0800

    refactor: 修改通用返回参数数值

--- src/main/java/com/github/jgzl/framework/vo/R.java
+++ src/main/java/com/github/jgzl/framework/vo/R.java
@@ -4,12 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * 通用数据返回对象
+ * @param <T>
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class R<T> {
+    /**
+     * 返回code
+     * @since 1.0
+     */
     private Integer code;
+    /**
+     * 返回信息
+     * @since 1.0
+     */
     private String message;
+    /**
+     * 返回数据
+     * @since 1.0
+     */
     private T data;
 
     public <T> R<T> of(Integer code,String message,T data){
